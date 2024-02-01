ad0090cf48589e3b945735cefecb030fc6e83e80
unknown <louis.pingot@live.be>
2024-02-01 19:29:25 +0100

    Controller Artist +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtistController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+//…
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+		->where('id', '[0-9]+')->name('artist.show');

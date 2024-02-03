ff89809c02a706e2cd4a3ae18f9e9a57e3577bb5
unknown <louis.pingot@live.be>
2024-02-03 17:11:02 +0100

    Controller Locality +template +route

--- resources/views/locality/show.blade.php
+++ resources/views/locality/show.blade.php
@@ -4,5 +4,11 @@
 
 @section('content')
     <h1>{{ ucfirst($locality->locality) }}</h1>
+    <ul>
+    @foreach($locality->locations as $location)
+        <li>{{ $location->designation }}</li>
+    @endforeach
+    </ul>
+
     <nav><a href="{{ route('locality.index') }}">Retour à l'index</a></nav>
 @endsection

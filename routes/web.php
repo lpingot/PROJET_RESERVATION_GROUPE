d217e93c8b15105bcd3de8736d43972e5ec660e8
unknown <louis.pingot@live.be>
2024-02-22 19:43:47 +0100

    Controller Show +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\TypeController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\LocationController;
-
+use App\Http\Controllers\ShowController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -66,3 +66,7 @@ Route::get('location', [LocationController::class, 'index'])->name('location.ind
 Route::get('location/{id}', [LocationController::class, 'show'])
         ->where('id', '[0-9]+')->name('location.show');
               
+Route::get('/show', [ShowController::class, 'index'])->name('show.index');
+Route::get('/show/{id}', [ShowController::class, 'show'])
+        ->where('id', '[0-9]+')->name('show.show');
+        

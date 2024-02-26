b798d1d90eed0cb00d4d6d72dc830b0427833614
unknown <louis.pingot@live.be>
2024-02-26 12:08:46 +0100

    Update templates Show (added artists’ list)

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -28,20 +28,47 @@
         @if($show->representations->count()>=1)
         <ul>
             @foreach ($show->representations as $representation)
-                <li>{{ $representation->when }} 
-                @if($representation->location)
-                ({{ $representation->location->designation }})
-                @elseif($representation->show->location)
-                ({{ $representation->show->location->designation }})
-                @else
-                (lieu à déterminer)
-                @endif
-                </li>
+                <li>{{ $representation->when }}</li>              
             @endforeach
         </ul>
         @else
         <p>Aucune représentation</p>
         @endif
+        
+        <h2>Liste des artistes</h2>
+        <ul>
+            @foreach ($show->artistTypes as $collaborateur)
+                <li>
+                    {{ $collaborateur->artist->firstname }}
+                    {{ $collaborateur->artist->lastname }}
+                    ({{ $collaborateur->type->type }})
+                </li>              
+            @endforeach
+        </ul>
+
+        <h2>Liste des artistes</h2>
+        <p><strong>Auteur:</strong>
+        @foreach ($collaborateurs['auteur'] as $auteur)
+            {{ $auteur->firstname }} 
+            {{ $auteur->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
+        <p><strong>Metteur en scène:</strong>
+        @foreach ($collaborateurs['scénographe'] as $scenographe)
+            {{ $scenographe->firstname }} 
+            {{ $scenographe->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
+        <p><strong>Distribution:</strong>
+        @foreach ($collaborateurs['comédien'] as $comedien)
+            {{ $comedien->firstname }} 
+            {{ $comedien->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
+
 
     </article>
 
